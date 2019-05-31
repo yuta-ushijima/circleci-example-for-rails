235b235002f6be6a01562f8dc7fde8e4c45d0a08
Yuta Ushijima <register@yuta-u.com>
2019-06-01 03:37:26 +0900

    Create Article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_183228) do
+ActiveRecord::Schema.define(version: 2019_05_31_183635) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "account"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_05_31_183228) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
